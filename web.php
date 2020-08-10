075cd0cf3c453b81fa40c177224f7eaf08a7ee23
Fairuz Firjatullah <fairuzfirjatullah3@gmail.com>
2020-08-10 18:39:54 +0700

    Tugas CRUD selesai

---
+++ web.php
@@ -0,0 +1,83 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+/*Route::get('/index', function () {
+    return view('index');
+});
+
+
+Route::get('/about',function()
+{ 
+return view('about'); 
+});
+*/
+
+Route::get('/welcome','AuthController@datang');
+Route::get('/register','AuthController@reg' );
+Route::get('/home','HomeController@home');
+Route::get('/admin','AuthController@admin');
+/*Route::get('/welcome',function()
+{
+
+    return view('welcome');
+}); 
+
+
+Route::get('/main',function()
+{  $jeneng = 'Sanbercode'; 
+
+    return view('main',['jeneng'=>$jeneng]);
+}); */
+
+Route::get('/', function()
+{
+    return view('tables'); 
+}); 
+
+Route::get('/data-tables',function()
+{
+
+    return view('datatables');
+}
+);
+
+Route::get('/admins','PertanyaanController@indx'); 
+
+Route::get('/mechtech',function()
+{
+
+    return view('mechtech');
+}
+);
+
+
+Route::get('/sourcethree',function()
+{
+
+    return view('sourcethree');
+}
+);
+
+Route::get('/source',function()
+{
+
+    return view('source');
+}
+);
+
+Route::get('/create','PertanyaanController@create');
+Route::post('/posts','PertanyaanController@store');
+
+
